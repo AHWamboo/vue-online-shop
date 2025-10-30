ad4b255ef36a3bad37dddb79096a7a294d18af65
wambookontakt@gmail.com <wambookontakt@gmail.com>
2025-10-30 18:30:41 +0000

    Cart page layout

--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -1,23 +1,17 @@
 <template>
   <div class="row">
-    <div class="col-6">
-      <h4>First column</h4>
-      <q-card class="my-card" flat bordered>
-        <q-card-section horizontal>
-          <q-card-section class="q-pt-xs">
-            <div class="text-overline">Product category</div>
-            <div class="text-h5 q-mt-sm q-mb-xs">Product title</div>
-            <div class="text-caption text-grey">Product short description</div>
-          </q-card-section>
-
-          <q-card-section class="col-5 flex flex-center">
-            <q-img
-              class="rounded-borders"
-              src="https://cdn.quasar.dev/img/parallax2.jpg"
-            />
-          </q-card-section>
-        </q-card-section>
-      </q-card>
+    <div class="col-6 column-equal-width">
+      <h4>Cart products</h4>
+      <div class="column">
+        <div class="col">Product</div>
+        <div class="col">Product</div>
+        <div class="col">Product</div>
+        <div class="col">Product</div>
+        <div class="col">Product</div>
+        <div class="col">Product</div>
+        <div class="col">Product</div>
+        <div class="col">Product</div>
+      </div>
     </div>
     <div class="col-6">
       <h4>Second column</h4>
@@ -28,8 +22,13 @@
 <script lang="ts"></script>
 
 <style scoped>
-.my-card {
-  width: 100%;
-  max-width: 350px;
+.column-equal-width .column > div {
+  padding: 10px 15px;
+  background: rgba(153, 153, 153, 0.15);
+  border: 1px solid rgba(153, 153, 153, 0.2);
+}
+
+.column-equal-width .column + .column {
+  margin-top: 1rem;
 }
 </style>
