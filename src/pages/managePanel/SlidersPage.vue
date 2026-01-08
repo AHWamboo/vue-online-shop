93e81289ca83b1cc0d627e29b5e74225a33daf57
wambookontakt@gmail.com <wambookontakt@gmail.com>
2026-01-08 22:22:08 +0000

    Cart page layout

--- src/pages/managePanel/SlidersPage.vue
+++ src/pages/managePanel/SlidersPage.vue
@@ -1,54 +1,85 @@
 <template>
   <q-page class="q-pa-md">
-    <div class="row justify-left">
-      <div class="col-xl-4 col-md-7 col-sm-12 col-xs-12">
-        <h2 class="brand-red">Index page slider</h2>
-        <q-form class="q-gutter-md" @submit="onSubmit">
-          <q-input
-            v-model="sliderHeader"
-            clearable
-            clear-icon="close"
-            filled
-            label="Slider header"
-            hint="Your slider header"
-            lazy-rules
-            :rules="[
-              (val) =>
-                (typeof val === 'string' && val.trim().length > 0) ||
-                'Please enter your slider header.',
-              (val) =>
-                !val ||
-                val.length <= 100 ||
-                'Please use maximum 100 characters.',
-            ]"
-          ></q-input>
+    <h2 class="brand-red q-mb-md">Index page slider</h2>
 
-          <q-input
-            v-model="sliderMessage"
-            filled
-            clearable
-            type="textarea"
-            label="Slider message"
-            hint="Please write your slider message"
-            lazy-rules
-            :rules="[
-              (val) => (val && val.length > 0) || 'Please type something',
-            ]"
-          ></q-input>
+    <div class="row">
+      <div class="col-auto">
+        <q-tabs v-model="tab" vertical class="text-teal">
+          <q-tab name="slider 1" icon="image" label="Slider 1" />
+          <q-tab name="slider 2" icon="image" label="Slider 2" />
+          <q-tab name="slider 3" icon="image" label="Slider 3" />
+        </q-tabs>
+      </div>
 
-          <div>
-            <q-file
-              v-model="file"
-              label="Pick one file"
-              filled
-              hint="Your slider image"
-            />
-          </div>
+      <div class="col q-ml-md">
+        <q-tab-panels
+          v-model="tab"
+          animated
+          swipeable
+          transition-prev="fade"
+          transition-next="fade"
+        >
+          <q-tab-panel name="slider 1">
+            <q-form class="q-gutter-md" @submit="onSubmit">
+              <q-input
+                v-model="sliderHeader"
+                clearable
+                clear-icon="close"
+                filled
+                label="Slider header"
+                hint="Your slider header"
+                lazy-rules
+                :rules="[
+                  (val) =>
+                    (typeof val === 'string' && val.trim().length > 0) ||
+                    'Please enter your slider header.',
+                  (val) =>
+                    !val ||
+                    val.length <= 100 ||
+                    'Please use maximum 100 characters.',
+                ]"
+              ></q-input>
 
-          <div>
-            <q-btn label="Update slider" type="submit" color="black"></q-btn>
-          </div>
-        </q-form>
+              <q-input
+                v-model="sliderMessage"
+                filled
+                clearable
+                type="textarea"
+                label="Slider message"
+                hint="Please write your slider message"
+                lazy-rules
+                :rules="[
+                  (val) => (val && val.length > 0) || 'Please type something',
+                ]"
+              ></q-input>
+
+              <div>
+                <q-file
+                  v-model="file"
+                  label="Pick one file"
+                  filled
+                  hint="Your slider image"
+                />
+              </div>
+
+              <div>
+                <q-btn
+                  label="Update slider"
+                  type="submit"
+                  color="black"
+                ></q-btn>
+              </div>
+            </q-form>
+          </q-tab-panel>
+
+          <q-tab-panel name="slider 2">
+            <div class="text-h6">forma slider 2</div>
+          </q-tab-panel>
+
+          <q-tab-panel name="slider 3">
+            <div class="text-h6">forma slider 3</div>
+          </q-tab-panel>
+        </q-tab-panels>
       </div>
     </div>
   </q-page>
@@ -60,10 +91,19 @@ import { ref } from "vue";
 const sliderHeader = ref("");
 const sliderMessage = ref("");
 const file = ref(null);
+const tab = ref("slider 1");
 
 function onSubmit() {
   console.log("onSubmit");
 }
 </script>
 
-<style></style>
+<style scoped>
+:deep(.q-tab__icon) {
+  color: #040f16 !important;
+}
+
+:deep(.q-tab__label) {
+  color: #040f16 !important;
+}
+</style>
